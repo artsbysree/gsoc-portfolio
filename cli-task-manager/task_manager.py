50438946eaf8d6b5c3b81c80040c345aebe98c00
Sree divya <160315585+artsbysree@users.noreply.github.com>
2026-01-02 17:15:09 +0530

    Update task_manager.py
    Add ability to create and save tasks

--- cli-task-manager/task_manager.py
+++ cli-task-manager/task_manager.py
@@ -1,5 +1,22 @@
+from storage import load_tasks, save_tasks
+
+def add_task():
+    task = input("Enter a new task: ")
+    tasks = load_tasks()
+    tasks.append({"task": task, "completed": False})
+    save_tasks(tasks)
+    print("Task added successfully!")
+
 def main():
-    print("Welcome to CLI Task Manager")
+    print("CLI Task Manager")
+    print("1. Add Task")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        add_task()
+    else:
+        print("Invalid option")
 
 if __name__ == "__main__":
     main()
